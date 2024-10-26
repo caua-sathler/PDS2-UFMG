3157f9daf11679acd4e1b8976b566490294bba3a
caua-sathler <cauasathlerufmg@gmail.com>
2024-10-26 19:55:43 -0300

    Creation of folders. Main folder and ADT Company folder

---
+++ VPL3-Bus_company_manager/main.cpp
@@ -0,0 +1,19 @@
+#include "Bus.hpp"
+#include "Company.hpp"
+
+#include <string>
+#include <iostream>
+
+int main(){
+    Bus onibus1("OLX123", 11);
+
+    onibus1.add_passengers(20);
+    
+
+
+
+
+
+
+    return 0;
+}
