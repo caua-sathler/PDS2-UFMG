2e2bea46ae947af5a6d0c6f0bc10063b7b2e5afa
caua-sathler <cauasathlerufmg@gmail.com>
2024-10-27 14:43:04 -0300

    Implementation of the ADT Company.

---
+++ VPL3-Bus_company_manager/Company.hpp
@@ -0,0 +1,21 @@
+#ifndef COMPANY_H
+#define COMPANY_H
+
+#include "Bus.hpp"
+#include <iostream>
+#include <string>
+
+class Company {
+private:
+    int num_buses;
+    Bus* buses[20];
+
+public:
+    Company(){};
+    Bus* add_bus(std::string lisense_plate, int max_capacity);
+    Bus* bus_search(std::string lisense_plate) const;
+    void print_bus_status() const;
+    ~Company(){};
+};
+
+#endif
