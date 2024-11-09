bda3104863461c2e3967f8c4f1e95d8ea2041ff9
caua-sathler <cauasathlerufmg@gmail.com>
2024-11-09 16:49:05 -0300

    Implementation of a function that invert a chain list.

---
+++ Virtual Programming Laboratories-VPLs/VPL4-Lists_&_makefile/src/list.cpp
@@ -0,0 +1,40 @@
+#include "list.hpp"
+
+#include <iostream>
+
+void List::insertNode(int data) {
+    Node* aux = new Node;
+    aux->data = data;
+    aux->next = nullptr;
+    if (head == nullptr) {
+        head = aux;
+        tail = aux;
+    } else {
+        tail->next = aux;
+        tail = aux;
+    }
+}
+
+void List::display() {
+    Node* aux = head;
+    while (aux != nullptr) {
+        cout << aux->data << " ";
+        aux = aux->next;
+    }
+    cout << endl;
+}
+
+// Você deverá implementar apenas esse método
+void List::inverte() {
+        Node* previous = nullptr;
+        Node* current = head;
+        Node* next = nullptr;
+
+        while(current != nullptr) {
+            next = current->next;
+            current->next = previous;
+            previous = current;
+            current = next;
+        }
+    head = previous;
+    }
