ec89571110ca53374fd5d4d2d842308816108a30
caua-sathler <cauasathlerufmg@gmail.com>
2024-11-23 09:16:35 -0300

    Creation: second function: min and max elements

--- Virtual Programming Laboratories-VPLs/VPL6-Students_control_platform/student.cpp
+++ Virtual Programming Laboratories-VPLs/VPL6-Students_control_platform/student.cpp
@@ -1,4 +1,5 @@
 #include "student.hpp"
+#include <algorithm>
 #include <iostream>
 #include <iomanip>
 #include <numeric>
@@ -10,20 +11,26 @@ Student::Student(std::string name, int id, std::vector<int> grades) {
     this->grades = grades;
 }
 
-float Student::average_grade(std::vector<int> grades) {
+void Student::average_grade(std::vector<int> grades) {
     int sum = 0;
     int num_elements = 0;
-    for(int i : grades) {
+    for(const auto& i : grades) {
         sum += grades[i];
         num_elements++;
     }
 
     float average = sum/num_elements;
-    return average;
+    std::cout << std::setprecision(2) << std::fixed << average << std::endl;
 }
 
-int Student::min_max_grade(std::vector<int> grades) {
-    
+void Student::min_max_grade(std::vector<int> grades) {
+    const auto min = std::min_element(begin(grades), end(grades));
+    const auto max = std::max_element(begin(grades), end(grades));
+
+    std::cout << *min << " " << *max << std::endl;
 }
+
+
+
 
 
